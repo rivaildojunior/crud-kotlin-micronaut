2f8a85f198b90a78501cffbd68ffb44f63cf1d9f
Rivaildo Sousa <rivaildojunior@yahoo.com.br>
2021-10-04 18:58:14 -0300

    Correções

--- src/main/kotlin/br/com/curso/Application.kt
+++ src/main/kotlin/br/com/curso/Application.kt
@@ -1,18 +1,7 @@
 package br.com.curso
 
 import io.micronaut.runtime.Micronaut.*
-import io.swagger.v3.oas.annotations.*
-import io.swagger.v3.oas.annotations.info.*
 
-@OpenAPIDefinition(
-        info = Info(
-               title = "demo",
-                version = "0.0"
-        )
-)
-object Api {
-
-}
 fun main(args: Array<String>) {
     build()
             .args(*args)
--- src/main/kotlin/br/com/curso/controller/HelloController.kt
+++ src/main/kotlin/br/com/curso/controller/HelloController.kt
@@ -4,16 +4,12 @@ import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Produces
-import io.swagger.v3.oas.annotations.responses.ApiResponse
 
 @Controller("/hello")
 class HelloController {
 
-    @ApiResponse(
-            responseCode = "201",
-            description = "Bar object correctly created")
     @Get
     @Produces(MediaType.TEXT_PLAIN)
-    fun olaMundo() = "Ola Mundo!!!!"
+    fun olaMundo() = "Olá, Mundo!!!!"
 
 }
